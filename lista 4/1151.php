a76071b6759f9396244f227f6dbc5265bc4d13ef
Higor <higor.a@aluno.ifsp.edu.br>
2023-03-18 01:15:33 -0300

    dfgdfg

--- lista 4/1151.php
+++ lista 4/1151.php
@@ -1,24 +1,23 @@
 <?php
 
-$n = readline(); 
+$n = readline();
 
-$ant2=0;
-$ant=1;
+$ant2 = 0;
+$ant = 1;
 
+if ($n == 1) {
+    echo "0\n";
+} else if ($n > 1) {
+    echo "$ant2";
+    echo " $ant";
 
-if($n==1){
-	echo "ant2\n";
-} else if($n>1){
-	echo "$ant2 $ant\n";
+    for ($i = 2; $i < $n; $i++) {
+        $soma = $ant + $ant2;
+        $ant2 = $ant;
+        $ant = $soma;
 
-
-for($i=2;$i<$n;$i++){
-	$soma = $ant + $ant2;
-	$ant2 = $ant;
-	$ant = $soma;
-	
-	echo " $soma\n"; 
-}
+        echo " $soma";
+    }
 }
 
 echo "\n";
