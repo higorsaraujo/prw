8bf15ff1492651923717729fb31f468184a40fc0
Higor <higor.a@aluno.ifsp.edu.br>
2023-03-28 09:38:16 -0300

    me

---
+++ aula2803/ex1.php
@@ -0,0 +1,13 @@
+<?php
+$hoje = date("w");
+$diaSemana[0] = "Domingo";
+$diaSemana[1] = "Segunda-feira";
+$diaSemana[3] = "Quarta-feira";
+$diaSemana[4] = "Quinta-feira";
+$diaSemana[5] = "Sexta-feira";
+$diaSemana[6] = "Sábado";
+echo "A variável $hoje contém o número: $hoje <br>";
+echo "Hoje é: $diaSemana[$hoje]";
+?>
+[2] = "Terça-feira";
+$diaSeman
