412b472045bcd89bcc40ccabd7de96297dfa4157
Higor <higor.a@aluno.ifsp.edu.br>
2023-04-25 09:24:44 -0300

    dfgdfgdf

--- formulario/form3/form3.php
+++ formulario/form3/form3.php
@@ -1,10 +1,84 @@
 <?php
 
-    $estado = $_GET['estado'];
+  $estado = $_GET['estado'];
 
-    switch ($estado) {
-        case "Acre":
-          
-          break;
+  switch ($estado) {
+    case 'Acre':
+        echo "Rio Branco";
+        break;
+    case 'Amazonas':
+        echo "Manaus";
+        break;
+    case 'Alagoas':
+        echo "Maceió";
+        break;
+    case 'Bahia':
+        echo "Salvador";
+        break;
+    case 'Ceará':
+        echo "Fortaleza";
+        break;
+    case 'Espírito Santo':
+        echo "Vitória";
+        break;
+    case 'Goiás':
+        echo "Goiânia";
+        break;
+    case 'Maranhão':
+        echo "São Luís";
+        break;
+    case 'Mato Grosso':
+        echo "Cuiabá";
+        break;
+    case 'Mato Grosso do Sul':
+        echo "Campo Grande";
+        break;
+    case 'Minas Gerais':
+        echo "Belo Horizonte";
+        break;
+    case 'Pará':
+        echo "Belém";
+        break;
+    case 'Paraíba':
+        echo "João Pessoa";
+        break;
+    case 'Paraná':
+        echo "Curitiba";
+        break;
+    case 'Pernambuco':
+        echo "Recife";
+        break;
+    case 'Piauí':
+        echo "Teresina";
+        break;
+    case 'Rio de Janeiro':
+        echo "Rio de Janeiro";
+        break;
+    case 'Rio Grande do Norte':
+        echo "Natal";
+        break;
+    case 'Rio Grande do Sul':
+        echo "Porto Alegre";
+        break;
+    case 'Rondônia':
+        echo "Porto Velho";
+        break;
+    case 'Roraima':
+        echo "Boa Vista";
+        break;
+    case 'Santa Catarina':
+        echo "Florianópolis";
+        break;
+    case 'São Paulo':
+        echo "São Paulo";
+        break;
+    case 'Sergipe':
+        echo "Aracaju";
+        break;
+    case 'Tocantins':
+        echo "Palmas";
+        break;
+    
+}
 
 ?>
