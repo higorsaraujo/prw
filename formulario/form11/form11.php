24e8f5309c2a5c087e4531bec3512835285125c1
Higor <higor.a@aluno.ifsp.edu.br>
2023-04-25 12:14:31 -0300

    gdfgdfgdf

--- formulario/form11/form11.php
+++ formulario/form11/form11.php
@@ -6,10 +6,10 @@
     $per = $_POST['per'];
     $mod = $_POST['mod'];
 
-    if($pront == ""){echo "Prontuário não pode estar em branco";}
-    if($nome == ""){echo "Nome não pode estar em branco";}
-    if($idade < 15 && $idade > 75){echo "Idade entre 15 e 75";}
-    if($per != "V" or $per != "N"){echo "Período V-Vespertino ou N-Noturno";}
-    if($mod < 0 && )
+    if($pront == ""){echo "Prontuário não pode estar em branco  ";}
+    if($nome == ""){echo "Nome não pode estar em branco  ";}
+    if($idade < 15 && $idade > 75){echo "Idade entre 15 e 75  ";}
+    if($per != "V" or $per != "N"){echo "Período V-Vespertino ou N-Noturno  ";}
+    if($mod < 0 && $mod > 4){echo "Modulo entre 1 e 4  ";}
 
 ?>
